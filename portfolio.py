2abdaa97eece34f0e8f18fdf054f3f37fc747b4c
Mohsin Shaikh <mohsin.shaikh324@gmail.com>
2023-07-05 23:02:10 -0700

    New HTML files loaded

---
+++ portfolio.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def main():
+    st.title("Mohsin Hamid Shaikh's Portfolio")
+
+    st.header("Projects")
+    st.markdown("- [Power BI Report](https://github.com/[your_username]/power-bi-report) - Generated Power BI Report for multiple companies with their Occupancy, HVAC, and AQI data.")
+    st.markdown("- [WhatsApp Chat Analyzer](https://github.com/[your_username]/whatsapp-chat-analyzer) - Detailed analysis of WhatsApp chats to perform sentiment analysis.")
+    st.markdown("- [Movie Recommendation System](https://github.com/[your_username]/movie-recommendation-system) - Built a model for predicting and recommending similar movies based on TMDB data.")
+    st.markdown("- [Occupancy Report Dashboard](https://github.com/[your_username]/occupancy-report-dashboard) - Created a dashboard to specify the client about workspace utilization.")
+    st.markdown("- [AQI Report Dashboard](https://github.com/[your_username]/aqi-report-dashboard) - Converted sensor-generated data into an AQI and thermal acquisition dashboard for offices.")
+
+    st.header("Experience")
+    "Add your experience details here"
+
+    st.header("Education")
+    "Add your education details here"
+
+    st.header("Achievements")
+    "Add your achievements here"
+
+    st.header("Certifications")
+    "Add your certifications here"
+
+if __name__ == "__main__":
+    main()
